3cb14ae791afaa1f4557587c32a9f5a6279ec6a1
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2022-08-31 04:58:27 +0700

    Update python.sh

--- websocket/python.sh
+++ websocket/python.sh
@@ -3,17 +3,17 @@
 # wisnucokrosatrio
 # ==========================================
 # Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/large/sae/websocket"
+crotaja="raw.githubusercontent.com/fisabiliyusri/main/websocket"
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/stunnelws https://${wisnuvpn}/stunnelws.py
+wget -q -O /usr/local/bin/stunnelws https://${crotaja}/stunnelws.py
 chmod +x /usr/local/bin/stunnelws
 
 # Installing Service
 cat > /etc/systemd/system/stunnelws.service << END
 [Unit]
-Description=SSH WEBSOCKET TLS ROUTING INDONESIA BY ZEROSSL
-Documentation=https://t.me/zerossl
+Description=SSH WEBSOCKET TLS
+Documentation=https://nekopoi.care
 After=network.target nss-lookup.target
 
 [Service]
@@ -35,14 +35,14 @@ systemctl enable stunnelws
 systemctl restart stunnelws
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/nontls https://${wisnuvpn}/nontls.py
+wget -q -O /usr/local/bin/nontls https://${crotaja}/nontls.py
 chmod +x /usr/local/bin/nontls
 
 # Installing Service
 cat > /etc/systemd/system/nontls.service << END
 [Unit]
-Description=SSH WEBSOCKET NON TLS ROUTING INDONESIA BY ZEROSSL
-Documentation=https://t.me/zerossl
+Description=SSH WEBSOCKET NON TLS
+Documentation=https://nekopoi.care
 After=network.target nss-lookup.target
 
 [Service]
@@ -64,13 +64,13 @@ systemctl enable nontls
 systemctl restart nontls
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/otls https://${wisnuvpn}/otls.py
+wget -q -O /usr/local/bin/otls https://${crotaja}/otls.py
 chmod +x /usr/local/bin/otls
 # Installing Service
 cat > /etc/systemd/system/otls.service << END
 [Unit]
-Description=OVPN WEBSOCKET TLS ROUTING INDONESIA BY ZEROSSL
-Documentation=https://t.me/zerossl
+Description=OVPN WEBSOCKET TLS
+Documentation=https://nekopoi.care
 After=network.target nss-lookup.target
 
 [Service]
@@ -98,8 +98,8 @@ chmod +x /usr/local/bin/openvpnws
 # Installing Service
 cat > /etc/systemd/system/openvpnws.service << END
 [Unit]
-Description=OVPN WEBSOCKET ROTING PENGKOL BY GANDRING
-Documentation=https://t.me/zerossl
+Description=OVPN WEBSOCKET
+Documentation=https://nekopoi.care
 After=network.target nss-lookup.target
 
 [Service]
